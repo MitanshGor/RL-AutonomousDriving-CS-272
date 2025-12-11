da4bed9b2bfcb53a5335761e7c7134e8fa51a586
henry4912 <108157454+henry4912@users.noreply.github.com>
2025-12-10 17:56:34 -0800

    Delete scripts/272-project-main directory

--- scripts/272-project-main/PPO_Model/__init__.py
+++
@@ -1,7 +0,0 @@
-# PPO Implementation from SB3:
-# https://github.com/DLR-RM/stable-baselines3/tree/master/stable_baselines3/ppo
-
-from stable_baselines3.ppo.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from stable_baselines3.ppo.ppo import PPO
-
-__all__ = ["PPO", "CnnPolicy", "MlpPolicy", "MultiInputPolicy"]
